e7fa05749c3f2bc377ec202c6fd1841941097d72
Gopikrishna Reddy <Gopikrishna.Reddy@akqa.com>
2020-08-14 17:59:10 +1200

    Add Edit frame and Custom child count on content editor

---
+++ Spark.Web/Processors/CustomUrlOnContentEditor.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Pipelines.GetContentEditorWarnings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Spark.Web.Processors
+{
+    public class CustomUrlOnContentEditor
+    {
+        public void Process(GetContentEditorWarningsArgs args)
+        {
+            if (args == null || args.Item == null)
+                return;
+
+            var warning = args.Add();
+            warning.Title = "Item Child Count";
+            warning.Text = args.Item.Children.Count.ToString();
+        }
+    }
+}
